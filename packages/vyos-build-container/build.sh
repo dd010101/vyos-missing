2027d333a784ad87a59d4b9baae9d411140d6db8
dd <dd@wx.tnyzeq.icu>
2024-06-06 07:27:18 +0200

    fixed vyos-build-container cache and improved cleanup

--- packages/vyos-build-container/build.sh
+++ packages/vyos-build-container/build.sh
@@ -8,10 +8,13 @@ docker rmi --force "vyos/vyos-build:${BRANCH_NAME}" || true
 docker rmi --force "${CUSTOM_DOCKER_REPO}/vyos/vyos-build:$BRANCH_NAME" || true
 
 echo "Building docker build container for branch ${BRANCH_NAME}..."
-docker build . -t "vyos/vyos-build:${BRANCH_NAME}"
+docker build --no-cache -t "vyos/vyos-build:${BRANCH_NAME}" .
 
 echo "Pushing ${BRANCH_NAME} image to registry ${CUSTOM_DOCKER_REPO}..."
 docker tag "vyos/vyos-build:${BRANCH_NAME}" "${CUSTOM_DOCKER_REPO}/vyos/vyos-build:${BRANCH_NAME}"
 docker push "${CUSTOM_DOCKER_REPO}/vyos/vyos-build:$BRANCH_NAME"
+
+echo "Cleaning local registry..."
+docker exec registry registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
 
 echo "Image ${BRANCH_NAME} was successfully built and pushed to registry ${CUSTOM_DOCKER_REPO}."
